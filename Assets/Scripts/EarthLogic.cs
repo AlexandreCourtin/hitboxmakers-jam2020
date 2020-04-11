e063f213cd379e9059d58fa75c726da4ed1c541d
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-04-11 20:16:39 +0200

    added exit input

--- Assets/Scripts/EarthLogic.cs
+++ Assets/Scripts/EarthLogic.cs
@@ -38,6 +38,10 @@ public class EarthLogic : MonoBehaviour
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
     }
 
     void FixedUpdate()

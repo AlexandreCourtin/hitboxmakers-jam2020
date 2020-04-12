14ae80c6735c7bf8509c11dfbfc67bf54ee28686
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-04-12 03:26:44 +0200

    added complete boss intro, very epic

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    float timer = 1.5f;
+    bool shake = false;
+    Camera cam;
+
+    void Start() {
+        cam = Camera.main;
+    }
+
+    void FixedUpdate() {
+        if (shake && timer > 0f) {
+            timer -= Time.fixedDeltaTime;
+            cam.transform.position = new Vector3(Random.Range(-.5f, .5f), Random.Range(-.5f, .5f), -10f);
+        } else if (shake) {
+            cam.transform.position = new Vector3(0f, 0f, -10f);
+            Destroy(this);
+        }
+    }
+
+    public void shakeCamera() {
+        shake = true;
+    }
+}
---
+++ Assets/Scripts/EndTextEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTextEvent : MonoBehaviour
+{
+    public void triggerEvent() {
+        GameObject.Find("FinalBoss").GetComponent<Animator>().SetBool("bossFight", true);
+    }
+}

1bdadf380d25a701acdede51785a6317accb9de7
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-04-12 15:37:31 +0200

    added musics

---
+++ Assets/Scripts/BossMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMusic : MonoBehaviour
+{
+    bool isPlaying = false;
+
+    void FixedUpdate() {
+        if (Game_Manager.GM.phase == 3 && !isPlaying) {
+            GetComponent<AudioSource>().Play();
+            isPlaying = true;
+        } else if (Game_Manager.GM.phase > 3) {
+            GetComponent<AudioSource>().Stop();
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/StartMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMusic : MonoBehaviour
+{
+    void FixedUpdate() {
+        if (Game_Manager.GM.phase > 1) {
+            GetComponent<AudioSource>().Stop();
+            Destroy(this);
+        }
+    }
+}
